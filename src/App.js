2d6b703b24e4d3a892f3f107ac7b941c20bfc015
viablitz <jari.kristensen@gmail.com>
2021-03-22 14:00:44 -0700

    Build fixed

--- src/App.js
+++ src/App.js
@@ -115,7 +115,7 @@ function ChatMessage(props) {
 
   return (<>
     <div className={`message ${messageClass}`}>
-      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} alt="" />
       <p>{text}</p>
     </div>
   </>)
